ee4d9a125b7a4ad8e042f43e4eb8ce344c65b005
dullsword <525649253@qq.com>
2021-07-05 04:58:10 +0800

    Site updated: 2021-07-05 04:58:08

---
+++ js/myhk.js
@@ -0,0 +1,6 @@
+var script = document.createElement("script");
+script.setAttribute("type", "text/javascript");
+script.setAttribute("id", "myhk");
+script.setAttribute("src", "https://myhkw.cn/api/player/162542684494");
+script.setAttribute("key", "162542684494");
+document.documentElement.appendChild(script);
