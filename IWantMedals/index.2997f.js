7f900ac224213c4a543548bb6437d71e8cf32419
dullsword <525649253@qq.com>
2022-02-24 05:07:07 +0800

    Site updated: 2022-02-24 05:07:04

--- IWantMedals/index.2997f.js
+++ IWantMedals/index.2997f.js
@@ -1,7 +1,7 @@
-System.register(["./application.bc35d.js"], function (_export, _context) {
+System.register(["./application.51d4f.js"], function (_export, _context) {
   "use strict";
 
-  var createApplication;
+  var createApplication, canvas, $p, bcr;
 
   function loadJsListFile(url) {
     return new Promise(function (resolve, reject) {
@@ -67,10 +67,15 @@ System.register(["./application.bc35d.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationBc35dJs) {
-      createApplication = _applicationBc35dJs.createApplication;
+    setters: [function (_application51d4fJs) {
+      createApplication = _application51d4fJs.createApplication;
     }],
     execute: function () {
+      canvas = document.getElementById('GameCanvas');
+      $p = canvas.parentElement;
+      bcr = $p.getBoundingClientRect();
+      canvas.width = bcr.width;
+      canvas.height = bcr.height;
       createApplication({
         loadJsListFile: loadJsListFile,
         fetchWasm: fetchWasm
